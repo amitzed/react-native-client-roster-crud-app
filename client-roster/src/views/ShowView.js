3760d7f6ad565c9680e584788eb2e76a89e554a2
amitzed <amitzmn@gmail.com>
2020-11-23 15:25:12 -0500

    Added more customer info and on show screen added icon to navigate to edit screen - not yet created

--- client-roster/src/views/ShowView.js
+++ client-roster/src/views/ShowView.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 import { Context } from '../context/ClientContext';
 
@@ -11,8 +12,22 @@ const ShowView = ({ navigation }) => {
   return (
     <View>
       <Text>{clientDetail.name}</Text>
+      <Text>{clientDetail.address}</Text>
+      <Text>(p) {clientDetail.phone}</Text>
+      <Text>(e) {clientDetail.email}</Text>
+      <Text>Balance Due: ${clientDetail.balance}</Text>
     </View>
   )
+}
+
+ShowView.navigationOptions = ({ navigation }) => {
+  return {
+    headerRight: () => (
+      <TouchableOpacity onPress={() => navigation.navigate('Edit')}>
+        <AntDesign name="edit" size={35} />
+      </TouchableOpacity>
+    ),
+  }
 }
 
 const styles = StyleSheet.create({});
