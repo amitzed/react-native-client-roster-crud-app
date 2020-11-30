17869ba1b3667809f3697d5e4cd23c1708b2be4b
amitzed <amitzmn@gmail.com>
2020-11-30 14:16:10 -0500

    Separated out the Form so that this component can be shared between the CreateView and EditView components

--- client-roster/src/views/EditView.js
+++ client-roster/src/views/EditView.js
@@ -1,51 +1,21 @@
-import React, { useContext, useState } from 'react';
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet } from 'react-native';
 
 import { Context } from '../context/ClientContext';
+import ClientDetailForm from '../components/ClientDetailForm';
 
 const EditView = ({ navigation }) => {
   const { state } = useContext(Context);
 
   const clientDetail = state.find((clientDetail) => clientDetail.id === navigation.getParam('id'));
 
-  const [name, setName] = useState(clientDetail.name);
-  const [email, setEmail] = useState(clientDetail.email);
-  const [phone, setPhone] = useState(clientDetail.phone);
-  const [address, setAddress] = useState(clientDetail.address);
-  const [balance, setBalance] = useState(clientDetail.balance);
-
-  return <View style={styles.container}>
-    <Text style={styles.label}>Edit Name:</Text>
-    <TextInput
-      style={styles.input}
-      value={name}
-      onChangeText={(newName) => setName(newName)}
+  return (
+    <ClientDetailForm
+      onSubmit={(name, email, phone, address, balance) => {
+        console.log(name, email, phone, address, balance);
+      }}
     />
-    <Text style={styles.label}>Edit Email:</Text>
-    <TextInput
-      style={styles.input}
-      value={email}
-      onChangeText={(newEmail) => setEmail(newEmail)}
-    />
-    <Text style={styles.label}>Edit Phone:</Text>
-    <TextInput
-      style={styles.input}
-      value={phone}
-      onChangeText={(newPhone) => setPhone(newPhone)}
-    />
-    <Text style={styles.label}>Edit Address:</Text>
-    <TextInput
-      style={styles.input}
-      value={address}
-      onChangeText={(newAddress) => setAddress(newAddress)}
-    />
-    <Text style={styles.label}>Edit Balance:</Text>
-    <TextInput
-      style={styles.input}
-      value={balance}
-      onChangeText={(newBalance) => setBalance(newBalance)}
-    />
-  </View>
+  )
 }
 
 const styles = StyleSheet.create({
